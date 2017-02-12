a2cb70232d3bba732fbb7784d95c53228937b051
Ben A. Morgan <ben@benmorgan.io>
2017-02-11 22:27:13 -0500

    get payouts migrated

--- db/migrate/20170211234447_create_payouts.rb
+++ db/migrate/20170211234447_create_payouts.rb
@@ -2,9 +2,9 @@ class CreatePayouts < ActiveRecord::Migration[5.0]
   def change
     create_table :payouts do |t|
       t.string :bitcoin_address, null: false
-      t.decimal :amount, scale: 10, null: false
+      t.decimal :amount, precision: 15, scale: 10, null: false
 
-      t.date_time :released_at
+      t.datetime :released_at
       t.references :released_by
 
       t.references :merge, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211225632) do
+ActiveRecord::Schema.define(version: 20170211234452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "citext"
+
+  create_table "approvals", force: :cascade do |t|
+    t.integer  "payout_id",  null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["payout_id"], name: "index_approvals_on_payout_id", using: :btree
+    t.index ["user_id"], name: "index_approvals_on_user_id", using: :btree
+  end
 
   create_table "merges", force: :cascade do |t|
     t.jsonb    "payload",    null: false
@@ -26,9 +35,22 @@ ActiveRecord::Schema.define(version: 20170211225632) do
     t.index ["merger_id"], name: "index_merges_on_merger_id", using: :btree
   end
 
+  create_table "payouts", force: :cascade do |t|
+    t.string   "bitcoin_address",                           null: false
+    t.decimal  "amount",          precision: 15, scale: 10, null: false
+    t.datetime "released_at"
+    t.integer  "released_by_id"
+    t.integer  "merge_id",                                  null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+    t.index ["merge_id"], name: "index_payouts_on_merge_id", using: :btree
+    t.index ["released_by_id"], name: "index_payouts_on_released_by_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.citext   "github_username", null: false
     t.string   "bitcoin_address"
+    t.datetime "adminified_at"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["github_username"], name: "index_users_on_github_username", unique: true, using: :btree
